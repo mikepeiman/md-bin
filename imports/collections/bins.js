144d47626fdc75c5c82df7492ce8d99375fbb9aa
Mike Peiman <mikepeiman@gmail.com>
2016-08-19 20:15:46 -0400

    installed codemirror, have a working markdown editor now, retains data on refresh, cool

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 
 Meteor.methods({
+
 	'bins.insert': function() {
 		return Bins.insert({
 			createdAt: new Date(),
@@ -9,10 +10,13 @@ Meteor.methods({
 			ownerId: this.userId
 		});
 	},
-
 	'bins.remove': function(bin) {
 		return Bins.remove(bin);
+	},
+	'bins.update': function(bin, content) {
+		return Bins.update(bin._id, { $set: { content } });
 	}
+
 });
 
 export const Bins = new Mongo.Collection('bins');
